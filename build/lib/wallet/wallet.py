daae32a7612f163a9ba32ac5f4ba5194f0a75935
Dinara Rysmakhanova <drysmakhanova@gmail.com>
2024-05-07 20:26:24 +0500

    message

---
+++ build/lib/wallet/wallet.py
@@ -0,0 +1,47 @@
+import argparse
+import os.path
+import pickle
+from .helper_classes import Wallet 
+from .helper_functions import balance, add, redact, find
+
+def main(*args, **kwargs):
+
+    # Получить и парсить аргументы с консольной строки
+    parser = argparse.ArgumentParser(usage='wallet.py [options]', description='Кошелек со счетом и транзакциями')
+    parser.add_argument('-b', '--balance', action='store_true', help="Вывод текущего баланса")
+    parser.add_argument('-n','--new', action='store_true', help="Добавить новую запись в транзакции")
+    parser.add_argument('-r','--redact', action='store_true', help="Редактировать запись в транзакциях")
+    parser.add_argument('-f','--find', action='store_true', help="Поиск по записям в транзакциях")
+    args = parser.parse_args()
+    arg = list(vars(args).keys())[list(vars(args).values()).index(True)]
+    
+    # Данные хранятся в одном файле, кошелек хранится в class объекте и пишется через pickle
+    filename = "wallet.txt"
+    
+    # Если есть существующий файл и он не пустой: открываем его. Если файл не существует: создается новый
+    # Файл читается и пишется через байт формат 
+    if os.path.isfile(filename) and os.path.getsize(filename) > 0:
+        with open(filename, "rb") as file:
+            wallet = pickle.load(file)
+    else:
+        with open(filename, "wb") as file:
+            wallet = Wallet()
+            pickle.dump(wallet, file)
+
+    # Если аргумент верный, отправляем на функции из helper_functions 
+    if arg == "balance":
+        balance(wallet)
+    elif arg == "new":
+        wallet = add(wallet)
+    elif arg == "find":
+        find(wallet.trans_list)
+    elif arg == "redact":
+        wallet = redact(wallet)
+
+    # Запись кошелька в файл после манипуляции
+    with open(filename, "wb") as file:
+        pickle.dump(wallet, file)
+
+if __name__ == '__main__':
+    main()
+
--- setup.py
+++ setup.py
@@ -8,26 +8,16 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="simple_wallet", 
-    version="1.0.0",  # Required
+    version="0.1.0", 
     description="Простой кошелек с балансом и транзакциями",  
     long_description=long_description,  
     url="https://github.com/drysmakhanova/simle_wallet", 
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(where="src"),  # Required
-    python_requires=">=3.7, <4",
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=["peppercorn"],  # Optional
+
+    packages=find_packages(include=['wallet', 'wallet.*']), 
+    scripts=['wallet\main.py','wallet\helper_classes.py','wallet\helper_functions.py'], 
+    entry_points={
+        'console_scripts': [
+            'simple_wallet=wallet.wallet:main'
+        ]
+    },
 )
